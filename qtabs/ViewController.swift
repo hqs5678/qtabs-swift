1691aa056d4b4384622152a68b8c745fd062bf6c
黄青松 <huangqingsong_5678@hotmail.com>
2017-08-24 18:08:23 +0800

    update

--- qtabs/ViewController.swift
+++ qtabs/ViewController.swift
@@ -20,18 +20,31 @@ class ViewController: UIViewController {
         
         tabs = QTabView(frame: self.view.frame)
         self.view.addSubview(tabs)
+        
         let blue = UIColor(hexString: "1c50f6")
+        // 设置指示器颜色
         tabs.indicatorColor = blue
+        // 设置正常状态下标题的颜色
         tabs.titleNormalColor = UIColor.darkGray
+        // 设置选中状态下标题的颜色
         tabs.titleSelectedColor = blue
+        // 设置指示器的高度
         tabs.indicatorHeight = 2
+        // 设置标题的背景颜色
         tabs.titleBackgroundColor = UIColor.lightGray.withAlphaComponent(0.3)
+        
+        // 自动布局，设置在父控件中的位置
         tabs.layoutInSuperview(20, 0, 0, 0)
+        
+        // 设置标题
         tabs.titles = ["推荐", "娱乐明星", "体育", "新热点", "新闻", "帅哥", "美女", "娱乐明星", "体育", "新热点", "新闻", "帅哥", "美女"]
         
+        // 模拟每个标签页的内容
         for i in 0 ..< tabs.titles.count {
             let vc = VC1()
             vc.index = "\(tabs.titles[i]) \(i)"
+            
+            // 已addChildViewController 的方式设置标签页内容
             self.addChildViewController(vc)
         }
     }
